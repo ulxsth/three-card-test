89de899d7a9d1ee253b7336cbe278370526a01d5
ulxsth <ulxsth@gmail.com>
2023-12-18 12:48:44 +0900

    refactor

--- scripts/script.js
+++ scripts/script.js
@@ -1,64 +1,52 @@
 import * as THREE from 'three';
-import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
 
-// シーンを作成
-const scene = new THREE.Scene();
-
-// カメラを作成
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-camera.position.z = 1;
-
-// レンダラーを作成
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0xbaf9ff, 1);
-document.body.appendChild(renderer.domElement);
-
-// 光源を作成
-const directionalLight = new THREE.DirectionalLight(0xffffff);
-directionalLight.position.set(1, 1, 1);
-
-// 自然光を追加
-const light = new THREE.AmbientLight(0xffffff, 1);
-scene.add(light);
-
-// 立方体を作成
-const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-const material = new THREE.MeshNormalMaterial();
-const cube = new THREE.Mesh(geometry, material);
-cube.rotation.x = Math.PI / 4;
-cube.rotation.y = Math.PI / 4;
-scene.add(cube);
-
+let scene, camera, renderer;
 let isMouseDown = false;
 let mouseX = 0;
-let rot = 0;
-document.addEventListener('mousedown', (event) => {
-  isMouseDown = true;
-  mouseX = event.pageX;
-});
+let rotate = 0;
 
-document.addEventListener('mouseup', () => {
-  isMouseDown = false;
-});
+function init() {
+  // シーンを作成
+  scene = new THREE.Scene();
 
-document.addEventListener('mousemove', (event) => {
-  if (isMouseDown) {
-    mouseX = event.pageX;
-  }
-});
+  // カメラを作成
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.z = 1;
 
-// オブジェクトを描画
+  // レンダラーを作成
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setClearColor(0xbaf9ff, 1);
+  document.body.appendChild(renderer.domElement);
+
+  // 光源を作成
+  const directionalLight = new THREE.DirectionalLight(0xffffff);
+  directionalLight.position.set(1, 1, 1);
+
+  // 自然光を追加
+  const light = new THREE.AmbientLight(0xffffff, 1);
+  scene.add(light);
+
+  // 立方体を作成
+  const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+  const material = new THREE.MeshNormalMaterial();
+  const cube = new THREE.Mesh(geometry, material);
+  cube.rotation.x = Math.PI / 4;
+  cube.rotation.y = Math.PI / 4;
+  scene.add(cube);
+}
+
+
 function animate() {
   const targetRot = (mouseX / window.innerWidth) * 360;
-  rot += (targetRot - rot);
+  rotate += (targetRot - rotate);
 
-  const radian = rot * Math.PI / 180;
+  const radian = rotate * Math.PI / 180;
   camera.position.x = 1 * Math.sin(radian);
   camera.position.z = 1 * Math.cos(radian);
   camera.lookAt(new THREE.Vector3(0, 0, 0));
@@ -66,4 +54,23 @@ function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
 }
-animate();
+
+function main() {
+  document.addEventListener('mousedown', () => {
+    isMouseDown = true;
+  });
+
+  document.addEventListener('mouseup', () => {
+    isMouseDown = false;
+  });
+
+  document.addEventListener('mousemove', (event) => {
+    if (isMouseDown) {
+      mouseX = event.pageX;
+    }
+  });
+
+  init();
+  animate();
+}
+document.addEventListener('DOMContentLoaded', main);
